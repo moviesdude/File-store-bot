9878fe349ab764ad609838715e7b7568c8325a35
MD Bots <133237308+moviesdude@users.noreply.github.com>
2023-08-30 21:25:21 +0530

    Update check_user_status.py

--- handlers/check_user_status.py
+++ handlers/check_user_status.py
@@ -23,6 +23,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You are Banned!.. Contact @GreyMatters_Bots_Discussion 😝", quote=True)
+            await cmd.reply_text("You are Banned!.. Contact @MD_TAMILAN_ADMIN_Bot 😝", quote=True)
             return
     await cmd.continue_propagation()
