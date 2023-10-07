f0715ac5d1fb7914cbda7dc49363886d60818d6c
MD Bots <133237308+moviesdude@users.noreply.github.com>
2023-10-07 09:54:41 +0530

    Update force_sub_handler.py

--- handlers/force_sub_handler.py
+++ handlers/force_sub_handler.py
@@ -32,7 +32,7 @@ async def handle_force_sub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/greymatters_bots_discussion).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/MD_Support_Group).",
                 disable_web_page_preview=True
             )
             return 400
